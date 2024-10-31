8fb046835dcaa77b0be8f37f58f35181872f497e
Daan de Graaf <d.degraaf@dictu.nl>
2024-10-31 17:50:32 +0100

    Abstracting proces functionality

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"os/exec"
-	"runtime"
-	"strings"
 )
 
 // Flags
@@ -61,29 +58,4 @@ func main() {
 	} else if !*quietFlag {
 		fmt.Printf("Killed process %s on port %s.\n", pid, port)
 	}
-}
-
-func getPID(port string) string {
-	var cmd *exec.Cmd
-	if runtime.GOOS == "windows" {
-		cmd = exec.Command("netstat", "-ano", "|", "findstr", ":"+port)
-	} else {
-		cmd = exec.Command("lsof", "-t", "-i:"+port)
-	}
-
-	out, err := cmd.Output()
-	if err != nil || len(out) == 0 {
-		return ""
-	}
-	return strings.TrimSpace(string(out))
-}
-
-func killPID(pid string) error {
-	var cmd *exec.Cmd
-	if runtime.GOOS == "windows" {
-		cmd = exec.Command("taskkill", "/PID", pid, "/F")
-	} else {
-		cmd = exec.Command("kill", "-9", pid)
-	}
-	return cmd.Run()
 }
